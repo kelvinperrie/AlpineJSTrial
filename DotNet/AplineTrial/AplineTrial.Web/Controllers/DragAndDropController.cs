7783923415e231c014c613e00e759bb48f27ef03
Kelvin <kelvin.perrie@tdhb.org.nz>
2025-06-17 09:49:58 +1200

    adding in complex drag and drop

---
+++ DotNet/AplineTrial/AplineTrial.Web/Controllers/DragAndDropController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplineTrial.Web.Controllers
+{
+    public class DragAndDropController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
